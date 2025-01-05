086d3694f724ecb573bba9c4fc3848da42d6d6a4
Andrew <phucducphan.ht@gmail.com>
2025-01-05 15:26:02 +0700

    update

---
+++ template/template.cpp
@@ -0,0 +1,17 @@
+// Written by Andrew - Phanh
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef BUG
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    return 0;
+};
