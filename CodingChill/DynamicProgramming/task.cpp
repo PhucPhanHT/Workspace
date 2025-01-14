995ece595f5c67ba6fe88335db0dcd8253b68f57
Andrew <phucducphan.ht@gmail.com>
2025-01-14 20:18:46 +0700

    update

--- CodingChill/DynamicProgramming/task.cpp
+++ CodingChill/DynamicProgramming/task.cpp
@@ -1,11 +1,29 @@
+// Written by Andrew - Phanh
 #include <bits/stdc++.h>
 
 using namespace std;
 
+#ifdef BUG
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
 signed main(){
-    int i;
-    for(i = 1; i <= n;i++){
-        cout << "HELLO WORLD";
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        int cnt = 0;
+        for(int i = 1;i < n;i++){
+            int b = i;
+            int a = n - i;
+            if (a < n) ++cnt;
+        }
+        cout << cnt << '\n';
     }
     return 0;
-}
+};
